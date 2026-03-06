98170db1181f8a1f59f9f02bbc16d04a0b13d929
Aryan-2204 <akadam2204@gmail.com>
2026-03-06 16:30:57 +0530

    week 1 prob 10

--- src/work.java
+++ src/work.java
@@ -1,21 +1,25 @@
 import java.util.*;
 
-public class TwoSumFraud {
+public class MultiLevelCache {
 
-    public void findTwoSum(int[] arr,int target){
+    LinkedHashMap<String,String> L1=new LinkedHashMap<>(10000,0.75f,true);
+    HashMap<String,String> L2=new HashMap<>();
 
-        HashMap<Integer,Integer> map=new HashMap<>();
+    public String getVideo(String id){
 
-        for(int i=0;i<arr.length;i++){
-
-            int complement=target-arr[i];
-
-            if(map.containsKey(complement)){
-
-                System.out.println("Pair found: "+arr[i]+" + "+complement);
-            }
-
-            map.put(arr[i],i);
+        if(L1.containsKey(id)){
+            return "L1 HIT";
         }
+
+        if(L2.containsKey(id)){
+            L1.put(id,L2.get(id));
+            return "L2 HIT → promoted to L1";
+        }
+
+        String data="VideoData-"+id;
+
+        L2.put(id,data);
+
+        return "L3 Database HIT";
     }
 }
